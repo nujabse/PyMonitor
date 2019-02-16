2f49a7bd9dce82c30eb92dae2dfef20f2f98fd43
MengZhu <h.p.zhumeng@gmail.com>
2019-02-17 01:42:09 +0800

    user input record worked.

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import QObject, pyqtSignal, Qt
+import csv
+import time
 from reporter import Ui_Dialog
 
 
-class myWindow(QtWidgets.QDialog, Ui_Dialog):
+class MyWindow(QtWidgets.QDialog, Ui_Dialog):
     def __init__(self):
-        super(myWindow, self).__init__()
+        super(MyWindow, self).__init__()
         self.setupUi(self)
         self.setWindowTitle("实验登记")
+        # set confirm button event connect
         self.confirm.pressed.connect(self.check_user_input)  # 注意这里不需要引号和括号，只需要写出函数名就可以了
         self.confirm.pressed.connect(self.check_machine_state)
+        self.confirm.pressed.connect(self.recorder)
+        self.confirm.pressed.connect(self.data_writer)
+        self.row = []
 
     def check_user_input(self):
         for item in [self.name_box, self.number_box]:
@@ -23,16 +28,35 @@ class myWindow(QtWidgets.QDialog, Ui_Dialog):
         else:
             if self.condition_right.checkState():
                 print("仪器正常，不错")
+                self.row.append("正常")
             if self.condition_wrong.checkState():
                 print("请务必描述仪器具体故障状况，发现故障的时间，并立即联系仪器负责人！")
+                self.row.append("故障")
 
-    def prompt_report(self):
-        if not self.condition_right.checkState():
-            print("请确定是否需要输入故障信息！")
+    def recorder(self):
+        """
+        record all the user input message to a list for data_writer to write to file
+        :return: self.row
+        """
+        print('Now recording user input')
+        self.row.append(time.asctime())
+        for item in [self.name_box, self.number_box]:
+            self.row.append(item.toPlainText())
+        if self.error_box.toPlainText():
+            self.row.append(self.error_box.toPlainText())
+        else:
+            print("User input all recorded!")
+
+    def data_writer(self):
+        print(self.row)
+        with open('monitor.csv', 'at') as f:
+            f_csv = csv.writer(f)
+            f_csv.writerow(self.row)
 
 
 app = QtWidgets.QApplication(sys.argv)
-application = myWindow()
+application = MyWindow()
 application.show()
-#  check if user has inputted all the necessary items.
+#  TODO: prohibit user from exiting through pressing ESC
+#  TODO: 解决无法输入中文的问题
 sys.exit(app.exec_())
